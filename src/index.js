f4a496ea7d69ffc1a5daaa4f132ad012e27771e9
k0robova <alonakorobova@ukr.net>
2023-11-07 22:52:02 +0200

    install

--- src/index.js
+++ src/index.js
@@ -8,3 +8,20 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
+
+// import ReactDOM from 'react-dom/client';
+
+const imageUrl =
+  'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640';
+const productPrice = 10.99;
+
+const product = (
+  <div>
+    <img src={imageUrl} alt="Tacos With Lime" width="640" />
+    <h2>Tacos With Lime</h2>
+    <p>Price: {productPrice}$</p>
+    <button type="button">Add to cart</button>
+  </div>
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(product);
